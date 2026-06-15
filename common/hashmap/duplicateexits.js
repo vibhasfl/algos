e4a135df90ede934b6b66da9bdf5ed977fc1eb0b
Vibhas <vibhas.fl@gmail.com>
2026-06-15 21:04:48 +0530

    Duplicate exists

--- common/hashmap/duplicateexits.js
+++ common/hashmap/duplicateexits.js
@@ -8,3 +8,20 @@
 
 // O(n²)?
 // O(n) with HashSet?
+
+function checkDuplicates(intArr){
+
+    let existing = new Set()
+    let hasDuplicate= false
+
+    for(let i=0;i<intArr.length;i++){
+        if(existing.has(intArr[i])){
+            hasDuplicate = true
+            break
+        }
+        existing.add(intArr[i])
+    }
+    return hasDuplicate
+}
+
+console.log(checkDuplicates([101,101,304,102]))
