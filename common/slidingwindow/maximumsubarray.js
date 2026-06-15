ce08da3238ea507e8531dbb1e5d9e1694329de5c
Vibhas <vibhas.fl@gmail.com>
2026-06-15 22:41:06 +0530

    sunarray

--- common/slidingwindow/maximumsubarray.js
+++ common/slidingwindow/maximumsubarray.js
@@ -1,14 +1,51 @@
-// Maximum Sum Subarray of Size K
+// You are given an integer array nums and an integer k. Find the maximum subarray sum of all the subarrays of nums that meet the following conditions:
 
-// Given:
+// The length of the subarray is k, and
+// All the elements of the subarray are distinct.
+// Return the maximum subarray sum of all the subarrays that meet the conditions. If no subarray meets the conditions, return 0.
 
-// [2,1,5,1,3,2]
-// K = 3
+// A subarray is a contiguous non-empty sequence of elements within an array.
 
-// Output:
+ 
 
-// 9
+// Example 1:
 
-// Subarray:
+// Input: nums = [1,5,4,2,9,9,9], k = 3
+// Output: 15
+// Explanation: The subarrays of nums with length 3 are:
+// - [1,5,4] which meets the requirements and has a sum of 10.
+// - [5,4,2] which meets the requirements and has a sum of 11.
+// - [4,2,9] which meets the requirements and has a sum of 15.
+// - [2,9,9] which does not meet the requirements because the element 9 is repeated.
+// - [9,9,9] which does not meet the requirements because the element 9 is repeated.
+// We return 15 because it is the maximum subarray sum of all the subarrays that meet the conditions
+// Example 2:
 
-// [5,1,3]
+// Input: nums = [4,4,4], k = 3
+// Output: 0
+// Explanation: The subarrays of nums with length 3 are:
+// - [4,4,4] which does not meet the requirements because the element 4 is repeated.
+// We return 0 because no subarrays meet the conditions.
+
+function getMaximumsubarray(intArr,count){
+
+    let maxSum=0
+    for(i=0;i<intArr.length-2;i++){
+        let a = intArr[i]
+        let b = intArr[i+1]
+        let c = intArr[i+2]
+
+        if(a!=b && b!=c){
+            sum = a+b+c 
+            maxSum= Math.max(sum,maxSum)
+        }else{
+            continue;
+        }
+    }
+
+    return maxSum
+
+
+}
+
+console.log(getMaximumsubarray([1,5,4,2,9,8,10],3))
