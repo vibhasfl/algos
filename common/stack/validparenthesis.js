7d936a070b969c6fcae4ec2cf06ed5fa6fe145b5
Vibhas <vibhas.fl@gmail.com>
2026-06-15 22:29:10 +0530

    Validparenthesis

--- common/stack/validparenthesis.js
+++ common/stack/validparenthesis.js
@@ -10,3 +10,38 @@
 
 // Why stack?
 // Time complexity?
+
+function checkParenthesis(myStr){
+
+    const pmap = {
+        "{":"}",
+        "(":")",
+        "[":"]"
+    }
+
+    let holder = []
+    const strchars = myStr.split("")
+    
+
+    for(let i=0;i<strchars.length;i++){
+
+        let holderlength = holder.length - 1
+       
+        if(i>0 && pmap[holder[holderlength]]==strchars[i]){
+       
+            
+            holder.pop()
+        }
+        else{
+            holder.push(strchars[i])
+        }
+        
+    }
+
+    return holder.length==0
+}
+
+console.log("output : ",checkParenthesis("({[]})"))
+console.log("output : ",checkParenthesis("(){}[]"))
+console.log("output : ",checkParenthesis("()"))
+console.log("output : ",checkParenthesis("(())"))
