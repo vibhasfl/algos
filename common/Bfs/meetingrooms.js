2fc520b4aab8d2cec778c369f97fc3d372848dbf
Vibhas <vibhas.fl@gmail.com>
2026-06-16 19:44:42 +0530

    Meeing rom - bfs

--- common/Bfs/meetingrooms.js
+++ common/Bfs/meetingrooms.js
@@ -1,14 +1,39 @@
-// Given:
+// Given a 2D array arr[][], where arr[i][0] is the starting time of ith meeting and arr[i][1] is the ending time of ith meeting, the task is to check if it is possible for a person to attend all the meetings such that he can attend only one meeting at a particular time.
 
-// [[0,30],[5,10],[15,20]]
+// Note: A person can attend a meeting if its starting time is greater than or equal to the previous meeting's ending time.
 
-// Can a single SRE attend all incidents?
+// Examples:
 
-// Output:
+// Input: arr[][] = [[1, 4], [10, 15], [7, 10]]
+// Output: true
+// Explanation: Since all the meetings are held at different times, it is possible to attend all the meetings.
+// Input: arr[][] = [[2, 4], [9, 12], [6, 10]]
+// Output: false
+// Explanation: Since the second and third meeting overlap, a person cannot attend all the meetings.
 
-// False
 
-// Follow-up:
+function canAttendAllMeeting(schedulesArr){
 
-// Sort by start time.
-// Check overlap.
+    schedulesArr.sort((a,b) => a[0]-b[0])
+    console.log(schedulesArr)
+    let lastMeetingTime = 0
+    
+  
+    for(let i=0;i<schedulesArr.length;i++){
+        
+        if(schedulesArr[i][0]<lastMeetingTime)
+        {
+            return false
+        }
+        else {
+            lastMeetingTime = schedulesArr[i][1]
+        }
+        
+    }
+
+    return true
+}
+
+console.log(canAttendAllMeeting([[1, 4], [10, 15], [7, 10]]))
+console.log(canAttendAllMeeting([[2, 4], [9, 12], [6, 10]]))
+console.log(canAttendAllMeeting([[5, 10], [10, 20]]))
