a3327e69f5fd1731d424880121ab346517534c86
Vibhas <vibhas.fl@gmail.com>
2026-06-21 20:05:01 +0530

    Need to solve kidane and maixum sum with contrian limit

---
+++ common/arrays/maximumsum-kidane.js
@@ -0,0 +1,16 @@
+// Given an integer array arr[], find the subarray (containing at least one element) which has the maximum possible sum, and return that sum.
+// Note: A subarray is a continuous part of an array.
+
+// Examples:
+
+// Input: arr[] = [2, 3, -8, 7, -1, 2, 3]
+// Output: 11
+// Explanation: The subarray [7, -1, 2, 3] has the largest sum 11.
+
+// Input: arr[] = [-2, -4]
+// Output: -2
+// Explanation: The subarray [-2] has the largest sum -2.
+
+// Input: arr[] = [5, 4, 1, 7, 8]
+// Output: 25
+// Explanation: The subarray [5, 4, 1, 7, 8] has the largest sum 25.
